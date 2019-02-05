bed99a2fa5ef3d0f9734f53e8a363d1f1a427ea0
David <>
2019-02-05 06:41:59 -0600

    Assignment 1: Broken manual component push

---
+++ src/app/warning-alert/warning-alert.component.spec.ts
@@ -0,0 +1,27 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { WarningAlertComponent } from './warning-alert.component';
+// import { async } from 'q';
+
+// describe('WarningAlertComponent', () => {
+//     let component: WarningAlertComponent;
+//     let fixture: ComponentFixture<WarningAlertComponent>;
+
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//             declarations: [ WarningAlertComponent ]
+//         })
+//         .compileComponents();
+//     }));
+
+//     beforeEach(() => {
+//         fixture = TestBed.createComponent(WarningAlertComponent);
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
+
+//     it('should create', () => {
+//         expect(component).toBeTruthy();
+//     });
+// });
+
