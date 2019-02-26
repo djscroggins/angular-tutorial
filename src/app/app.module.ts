5ef8056c7c6597f852078da1930660c5f68220f8
David Scroggins <david.j.scroggins@gmail.com>
2019-02-25 18:46:43 -0600

    2.30 Two way databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {AppComponent} from './app.component';
+import {ServerComponent} from './server/server.component';
+import {ServersComponent} from './servers/servers.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from './success-alert/success-alert.component';
 
 
 @NgModule({
@@ -24,4 +24,5 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
