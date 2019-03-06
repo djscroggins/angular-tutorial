8cdd7738e4b0f43fb905881c5cbab45f3f91ce79
David Scroggins <david.j.scroggins@gmail.com>
2019-03-06 06:04:12 -0600

    2.28 Default, None, Native View Encapsulation

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -5,7 +5,7 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
   // This will make this module's CSS global
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated // default, also None, Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
